f3f68c75ff4e07506d2da099ec93d730e49689a5
Varun Kumar <kumarvarun9811@gmail.com>
2025-12-22 22:13:08 +0530

    3 Sum Problem Added

---
+++ arrays/medium/3sum/index.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    vector<vector<int>> result;
+    int n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        // Skip duplicate elements
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        int left = i + 1;
+        int right = n - 1;
+
+        while (left < right)
+        {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum == 0)
+            {
+                result.push_back({nums[i], nums[left], nums[right]});
+
+                // Skip duplicates for left and right
+                while (left < right && nums[left] == nums[left + 1])
+                    left++;
+                while (left < right && nums[right] == nums[right - 1])
+                    right--;
+
+                left++;
+                right--;
+            }
+            else if (sum < 0)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+    }
+    return result;
+}
+int main()
+{
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = threeSum(nums);
+    for (auto &triplet : result)
+    {
+        cout << "[ ";
+        for (int x : triplet)
+            cout << x << " ";
+        cout << "]\n";
+    }
+    return 0;
+}
