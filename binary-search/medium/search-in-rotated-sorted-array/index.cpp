8ff3b9ad8c861690a478a671d487ae3e54756c33
Varun Kumar <kumarvarun9811@gmail.com>
2025-12-30 21:53:34 +0530

    search in rotated sorted arrray

---
+++ binary-search/medium/search-in-rotated-sorted-array/index.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int search(vector<int> &nums, int target)
+{
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        // Found target
+        if (nums[mid] == target)
+            return mid;
+
+        // Left half is sorted
+        if (nums[low] <= nums[mid])
+        {
+            if (nums[low] <= target && target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        // Right half is sorted
+        else
+        {
+            if (nums[mid] < target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int target = 0;
+    int result = search(nums, target);
+    cout << result;
+    return 0;
+}
