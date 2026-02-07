e7a2ee441ef8365ba0b4d94dacf03aee9535a983
Varun Kumar <kumarvarun9811@gmail.com>
2026-02-07 14:42:46 +0530

    1653 Minimum Deletions to Make String Balanced

---
+++ strings/medium/1653-minimum-deletions-to-make-string-balanced/index.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int minimumDeletions(string s)
+{
+    int stringLength = s.size();
+    vector<int> dp(stringLength + 1, 0);
+    int bCount = 0;
+    for (int i = 1; i <= stringLength; i++)
+    {
+        if (s[i - 1] == 'b')
+        {
+            dp[i] = dp[i - 1];
+            bCount++;
+        }
+        else
+        {
+            dp[i] = min(dp[i - 1] + 1, bCount);
+        }
+    }
+    return dp[stringLength];
+}
+int main()
+{
+    string s = "aababbab";
+    cout << minimumDeletions(s) << endl;
+    return 0;
+}
